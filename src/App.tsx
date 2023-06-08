7cb18b93ddaa05e6dc4e056808ef781c010fede1
Rino Tovar <>
2023-06-08 13:36:26 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex items-center justify-center w-screen h-screen">
+      <h1 className="text-4xl">Hello world!</h1>
+    </main>
+  )
+}
+
+export default App
